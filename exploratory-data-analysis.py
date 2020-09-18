4d70d03f3d2f742494ff07d28ad8fc2e10a65664
Christoph Jabs <christoph@jabs-family.de>
2020-09-18 09:44:09 +0300

    Some plots of the wave height

--- exploratory-data-analysis.py
+++ exploratory-data-analysis.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
+pd.options.plotting.backend = 'plotly'
 
 # %% [markdown]
 # ## Wave Data
@@ -22,26 +24,39 @@ wave_data
 wave_data.describe()
 
 # %%
+print('Percentages of missing data:')
+wave_data.isnull().mean()
+
+# %%
 station_data
 
 # %%
-# Indexing helpers
-idx = pd.IndexSlice
-wave_height = idx[:, 'Wave height (m)']
-wave_dir = idx[:, 'Direction of waves (deg)']
+# Divide wave height and direction
+wave_height = wave_data.iloc[:,1::2]
+wave_height.columns = wave_height.columns.droplevel(1)
+wave_dir = wave_data.iloc[:,::2]
+wave_dir.columns = wave_dir.columns.droplevel(1)
 
 # %%
 # Plot wave height
-wave_data.loc[:,wave_height].plot()
-plt.show()
+fig = wave_height.plot()
+fig.update_layout(title='Wave height')
+fig.show()
 
 # %%
 # Plot one month of wave height
-wave_data.loc['2020-01',wave_height].plot()
-plt.gca().set_xlim('2020-01-01', '2020-02-01')
-plt.show()
+fig = wave_height.loc['2020-01',:].plot()
+fig.update_layout(title='Wave height')
+fig.show()
 
 # %%
-# Plot missing values
-sns.heatmap(wave_data.loc[:,wave_height].isnull().values)
-plt.show()
+x = np.array(wave_height.index).astype('datetime64[m]')
+y = wave_height.columns
+z = np.invert(wave_height.isnull().values).astype(int).T
+colorsc = [[0, 'rgb(194,59,34)'],
+            [0.5, 'rgb(194,59,34)'], 
+            [0.5, 'rgb(0,179,30)'],
+            [1, 'rgb(0,179,30)']]
+fig = go.Figure(data=go.Heatmap(x=x, y=y, z=z, colorscale=colorsc, colorbar=dict(tickmode='array', tickvals=[0,1], ticktext=['null', 'value'])))
+fig.update_layout(title='Missing values over time', xaxis=dict(side='top'))
+fig.show()
