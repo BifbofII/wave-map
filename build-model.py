5c86dbf6d059a9c7330b2902fe278bf0ca4b838b
Christoph Jabs <christoph@jabs-family.de>
2020-09-29 12:13:19 +0300

    Added more ridge regression models
    * Added ridge regression with reduced inputs
    * Added ridge regression with additional features

--- build-model.py
+++ build-model.py
@@ -23,8 +23,14 @@ weather_data = pd.read_csv('data/weather_data.csv', header=[0,1], index_col=0, p
 data = pd.concat([wave_data, weather_data], axis=1, join='inner')
 
 # %%
+# Add squared wind speed
+sq_dat = data.loc[:,(slice(None),'wind_speed')]**2
+sq_dat.columns = pd.MultiIndex.from_tuples([(b, p+'_sq') for b, p in sq_dat.columns])
+data = pd.concat([data, sq_dat], axis=1)
+
+# %%
 # Add lagging values
-lagging_vars = ['10u', '10v', '2t', 'sp']
+lagging_vars = ['10u', '10v', '2t', 'sp', 'wind_speed', 'wind_speed_sq']
 lagging_times = [datetime.timedelta(hours=h) for h in range(1, 20)]
 dat = [data]
 for var in lagging_vars:
@@ -422,7 +428,7 @@ coef[(coef['wave_u'] == 0) & (coef['wave_v'] == 0)].index
 # Why is the coefficient of the longest lag always that high?
 
 # %% [markdown]
-# ## Lasso Regression - Wind Only - Shorter Lag
+# ## Lasso Regression - Wind Only - More Lag
 # Lasso Regression for selecting which wind lag is important.
 #
 # Inputs:
@@ -483,3 +489,125 @@ coef[(coef['wave_u'] == 0) & (coef['wave_v'] == 0)].index
 # %% [markdown]
 # Here the strongest influence seems to be from the parameters lagging by 2 and 4 hours.
 # Will keep 2 and 4 hour lag.
+
+# %% [markdown]
+# ## Ridge Regression - Wind only
+# Ridge regression from Wind Vector, Wind Speed and Wind Speed Squared
+# Lag for 2 hours and 4 hours.
+#
+# Inputs:
+# * 10u
+# * 10v
+# * 10u_lag2
+# * 10v_lag2
+# * 10u_lag4
+# * 10v_lag4
+#
+#
+# Outputs:
+# * wave_u
+# * wave_v
+
+# %%
+# Build input and output matrices
+base_vars = ['10u', '10v']
+input_vars = base_vars.copy()
+for l in [2, 4]:
+    input_vars.extend([v + '_lag' + str(l) for v in base_vars])
+input_vars.sort()
+output_vars = ['wave_u', 'wave_v']
+
+test_dropped = test.dropna()
+X_test = test_dropped[input_vars].values
+Y_test = test_dropped[output_vars].values
+
+X_train = None
+Y_train = None
+for i, buoy in enumerate(train.columns.levels[0].drop(test_buoy)):
+    buoy_dat = train[buoy].dropna()
+    if X_train is None:
+        X_train = buoy_dat[input_vars].values
+        Y_train = buoy_dat[output_vars].values
+    else:
+        X_train = np.concatenate([X_train, buoy_dat[input_vars].values])
+        Y_train = np.concatenate([Y_train, buoy_dat[output_vars].values])
+
+# %%
+# Train model
+alphas = np.logspace(-2, 5, num=20)
+pipe = Pipeline([('scaler', StandardScaler()), ('regression', RidgeCV())])
+pipe.fit(X_train, Y_train)
+print('Chosen regularization parameter: alpha={:.3f}'.format(pipe.named_steps['regression'].alpha_))
+print('Train accuracy of the model: {:.3f}'.format(pipe.score(X_train, Y_train)))
+
+# %%
+# Test model
+print('Test accuracy of the model: {:.3f}'.format(pipe.score(X_test, Y_test)))
+
+# %%
+# Model coefficiens
+describe_regression(pipe, input_vars, output_vars)
+
+# %% [markdown]
+# ## Ridge Regression - Wind only - More Features
+# Ridge regression from Wind Vector, Wind Speed and Wind Speed Squared
+# Lag for 2 hours and 4 hours.
+#
+# Inputs:
+# * 10u
+# * 10v
+# * wind_speed
+# * wind_speed_sq
+# * 10u_lag2
+# * 10v_lag2
+# * wind_speed_lag2
+# * wind_speed_sq_lag2
+# * 10u_lag4
+# * 10v_lag4
+# * wind_speed_lag4
+# * wind_speed_sq_lag4
+#
+#
+# Outputs:
+# * wave_u
+# * wave_v
+
+# %%
+# Build input and output matrices
+base_vars = ['10u', '10v', 'wind_speed', 'wind_speed_sq']
+input_vars = base_vars.copy()
+for l in [2, 4]:
+    input_vars.extend([v + '_lag' + str(l) for v in base_vars])
+input_vars.sort()
+output_vars = ['wave_u', 'wave_v']
+
+test_dropped = test.dropna()
+X_test = test_dropped[input_vars].values
+Y_test = test_dropped[output_vars].values
+
+X_train = None
+Y_train = None
+for i, buoy in enumerate(train.columns.levels[0].drop(test_buoy)):
+    buoy_dat = train[buoy].dropna()
+    if X_train is None:
+        X_train = buoy_dat[input_vars].values
+        Y_train = buoy_dat[output_vars].values
+    else:
+        X_train = np.concatenate([X_train, buoy_dat[input_vars].values])
+        Y_train = np.concatenate([Y_train, buoy_dat[output_vars].values])
+
+# %%
+# Train model
+alphas = np.logspace(-2, 5, num=20)
+pipe = Pipeline([('scaler', StandardScaler()), ('regression', RidgeCV())])
+pipe.fit(X_train, Y_train)
+print('Chosen regularization parameter: alpha={:.3f}'.format(pipe.named_steps['regression'].alpha_))
+print('Train accuracy of the model: {:.3f}'.format(pipe.score(X_train, Y_train)))
+
+# %%
+# Test model
+print('Test accuracy of the model: {:.3f}'.format(pipe.score(X_test, Y_test)))
+
+# %%
+# Model coefficiens
+describe_regression(pipe, input_vars, output_vars)
