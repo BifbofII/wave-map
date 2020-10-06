c3af93718485e460f65524aa3101e591c1ffaec8
Christoph Jabs <christoph@jabs-family.de>
2020-10-06 10:09:56 +0300

    Added combined predictor

--- predmod.py
+++ predmod.py
@@ -10,7 +10,7 @@ from abc import ABC, abstractmethod
 class WavePredictor(ABC):
     """An abstract base class for wrapper classes around a wave model"""
 
-    def __init__(self, model_path, grib_data):
+    def __init__(self, model_path, grib_data=None):
         """
         Abstract function for initializing a predictor
 
@@ -20,7 +20,10 @@ class WavePredictor(ABC):
         with open(model_path, 'rb') as f:
             self.model = pickle.load(f)
 
-        self.grib_data = pygrib.open(grib_data)
+        if grib_data is not None:
+            self.grib_data = pygrib.open(grib_data)
+        else:
+            self.grib_data = None
 
     def __del__(self):
         """Destructor closing the grib file"""
@@ -39,6 +42,8 @@ class WavePredictor(ABC):
             Also the lats, lons and times are also returned.
         :raises ValueError: if the location or time are out of the range in the data
         """
+        if self.grib_data is None:
+            raise ValueError('No data file was specified on initialization')
         if time is None:
             times = np.unique([datetime.datetime(grb.year, grb.month, grb.day, grb.hour) for grb in self.grib_data])
             self.grib_data.seek(0)
@@ -64,7 +69,10 @@ class WavePredictor(ABC):
         for p in short_names:
             series = []
             for t in times:
-                grb = self.grib_data.select(shortName=p, year=t.year, month=t.month, day=t.day, hour=t.hour)[0]
+                try:
+                    grb = self.grib_data.select(shortName=p, year=t.year, month=t.month, day=t.day, hour=t.hour)[0]
+                except IndexError:
+                    raise ValueError('The time or parameter was not found in the grib file')
                 if location is None:
                     series.append(grb.values)
                 else:
@@ -79,7 +87,8 @@ class WavePredictor(ABC):
         Predict the wave height at a location and time
 
         :param location: an (optional) location to do the prediction at as a (lat,lon) tuple
-        :param time: an (optional) time to do the prediction for as datetime
+        :param time: an (optional) time to do the prediction for as datetime.
+            A list or array of times is also possible.
         :returns: a numpy array with different dimensions, depending on the call
             If location was specified, a array of (time x params) is returned.
             If location was not specified, but time was specified, a (lat x lon x params) grid is returned
@@ -96,12 +105,15 @@ class SGDWaveModel(WavePredictor):
     def predict(self, location=None, time=None):
         # Load data
         now, lats, lons, times = self.extract_grib_data(location=location, time=time, short_names=['10u', '10v'])
+        now = now.copy()
         lag_2, _, _, _ = self.extract_grib_data(location=location, time=time-datetime.timedelta(hours=2),
             short_names=['10u', '10v'])
+        lag_2 = lag_2.copy()
         lag_2.index = lag_2.index + datetime.timedelta(hours=2)
         lag_2.columns = [c + '_lag2' for c in lag_2.columns]
         lag_4, _, _, _ = self.extract_grib_data(location=location, time=time-datetime.timedelta(hours=4),
             short_names=['10u', '10v'])
+        lag_4 = lag_4.copy()
         lag_4.index = lag_4.index + datetime.timedelta(hours=4)
         lag_4.columns = [c + '_lag4' for c in lag_4.columns]
         # Join data
@@ -126,16 +138,52 @@ class SGDWaveModel(WavePredictor):
                     row.append(self.model.predict(X)[np.newaxis,np.newaxis,:,np.newaxis])
                 pred.append(np.concatenate(row, axis=1))
             pred = np.concatenate(pred, axis=0)
-            if time is not None:
+            if pred.shape[2] == 1:
                 pred = pred[:,:,0,:]
+                if pred.shape[2] == 1:
+                    pred = pred[:,:,0]
+            elif pred.shape[3] == 1:
+                pred = pred[:,:,:,0]
             return pred, lats, lons, times
         else:
             X = data[input_vars].values
-            return self.model.predict(X)[:,np.newaxis].T, lats, lons, times
+            return self.model.predict(X).T, lats, lons, times
 
+
+class JoinedWaveModel(WavePredictor):
+    """A model joining the wave height and direction prediction"""
+
+    def __init__(self, height_model, u_model, v_model, grib_data):
+        """Initialize the predictor by loading all models and the grib data"""
+        super().__init__(height_model, grib_data)
+        del self.model
+
+        self.height_model = SGDWaveModel(height_model)
+        self.height_model.extract_grib_data = self.extract_grib_data
+
+        self.u_model = SGDWaveModel(u_model)
+        self.u_model.extract_grib_data = self.extract_grib_data
+
+        self.v_model = SGDWaveModel(v_model)
+        self.v_model.extract_grib_data = self.extract_grib_data
+
+    def predict(self, location=None, time=None):
+        # Get predictions of different models
+        height, lats, lons, times = self.height_model.predict(location, time)
+        u, _, _, _ = self.u_model.predict(location, time)
+        v, _, _, _ = self.v_model.predict(location, time)
+
+        # Scale factor for u and v components to include predicted wave height
+        scale_factor = height * np.sqrt(u**2 + v**2)
+
+        # Calc returns
+        u = u * scale_factor
+        v = v * scale_factor
+        res = np.concatenate([np.expand_dims(u, len(u.shape)), np.expand_dims(v, len(v.shape))], axis=len(u.shape)-1)
+
+        return res, lats, lons, times
 
 # Main for debugging
 if __name__ == '__main__':
-    wh = SGDWaveHeight('models/wave-height-model.pkl', 'data/weather_data.grib')
-    print(wh.predict(time=datetime.datetime(2019, 2, 15, 6)))
-    print(wh.predict(location=(60.123,24.972)))
+    wm = JoinedWaveModel('models/wave-height-model.pkl', 'models/wave-u-model.pkl', 'models/wave-v-model.pkl', 'data/weather_data.grib')
+    print(wm.predict(location=(60.123,24.972), time=datetime.datetime(2019, 2, 15, 6)))
